49d948b47d16b43e7e275d0088c6963263e62ac3
Triethoccode <tranminhtriet2406@gmail.com>
2025-11-24 21:41:48 +0700

    Update HomePage.js

--- frontend/src/pages/Home/HomePage.js
+++ frontend/src/pages/Home/HomePage.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./HomePage.css";
 import HeroSection from "./hero/hero";
 import HomeRecommendations from "./Recommendations/HomeRecommendations";
+import CreateTripForm from "../../components/CreateTripForm";
 
-export default function HomePage({ savedIds, handleToggleSave }) { 
+export default function HomePage({ savedIds, handleToggleSave }) {
+  const [allDestinations, setAllDestinations] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [selectedDestination, setSelectedDestination] = useState(null);
+
+  // Load all destinations
+  useEffect(() => {
+    axios.get("/api/destinations")
+      .then(res => setAllDestinations(res.data))
+      .catch(console.error);
+  }, []);
+
+  const handleCreateTrip = (destinationObj) => {
+    setSelectedDestination(destinationObj); // lưu trực tiếp object
+    setShowForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setSelectedDestination(null);
+    setShowForm(false);
+  };
+
   return (
     <div className="home-container">
       <HeroSection />
 
-      {/* Title riêng */}
       <h2 className="recommendations-title">Recommended Destinations</h2>
 
-      {/* Container card */}
       <div className="home-recommendations-container">
-        <HomeRecommendations 
-          savedIds={savedIds} 
-          handleToggleSave={handleToggleSave} 
+        <HomeRecommendations
+          savedIds={savedIds}
+          handleToggleSave={handleToggleSave}
+          onCreateTrip={handleCreateTrip}
+          destinations={allDestinations}
         />
+
+        {showForm && selectedDestination && (
+          <CreateTripForm
+            initialDestination={selectedDestination}
+            onClose={handleCloseForm}
+          />
+        )}
       </div>
     </div>
   );
