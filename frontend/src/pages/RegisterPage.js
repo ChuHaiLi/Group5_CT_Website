3f2a3a80d5c2c952964bbed2e4463c5ba1430189
Triethoccode <tranminhtriet2406@gmail.com>
2025-11-25 11:01:33 +0700

    Update RegisterPage.js

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "../untils/axios";
 import { toast } from "react-toastify";
 import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 import "../styles/AuthForm.css";
