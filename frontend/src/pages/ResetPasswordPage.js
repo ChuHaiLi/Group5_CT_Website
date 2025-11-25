32b66987a2e7671345dd56aa0c0fa47defa02fd9
Triethoccode <tranminhtriet2406@gmail.com>
2025-11-25 11:01:09 +0700

    Update ResetPasswordPage.js

--- frontend/src/pages/ResetPasswordPage.js
+++ frontend/src/pages/ResetPasswordPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios from "../untils/axios";
 import { toast } from "react-toastify";
 import { useSearchParams, useNavigate, Link } from "react-router-dom";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
